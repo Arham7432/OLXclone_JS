14f5fd583ef7948448cf259fd3a6300767335af3
async arhum 🎮 <154443094+Arham7432@users.noreply.github.com>
2024-11-19 09:50:07 +0500

    Add files via upload

---
+++ OLX_cloned/app.js
@@ -0,0 +1,180 @@
+let superBikes = [
+    bike1 = {
+        price: "Rs 1,200,000",
+        model: "Kawasaki Ninja ZX-10R 2023 Edition",
+        location: "Sharifabad, Karachi",
+        timePast: "2 weeks ago",
+        image: "assets/bike.png",
+    },
+    bike2 = {
+        price: "Rs 1,500,000",
+        model: "Yamaha R1 | Model 2022",
+        location: "Canal View Housing Scheme, Gujranwala",
+        timePast: "1 day ago",
+        image: "assets/bike2.png",
+    },
+    bike3 = {
+        price: "Rs 2,000,000",
+        model: "Ducati Panigale V4",
+        location: "Abid Market, Lahore",
+        timePast: "1 week ago",
+        image: "assets/bike3.png",
+    },
+    bike4 = {
+        price: "Rs 850,000",
+        model: "Suzuki Hayabusa GSX1300R",
+        location: "Mustafa Town, Lahore",
+        timePast: "6 hours ago",
+        image: "assets/bike4.png",
+    }
+];
+
+// For pyari Bike 1 🚲
+b1i.src = bike1.image
+bike1Price.innerHTML = bike1.price;
+bike1Model.innerHTML = bike1.model;
+b1l.innerHTML = bike1.location
+b1tp.innerHTML = bike1.timePast
+
+// For pyari Bike 2 🚲
+b2i.src = bike2.image
+b2p.innerHTML = bike2.price
+b2m.innerHTML = bike2.model
+b2l.innerHTML = bike2.location
+b2tp.innerHTML = bike2.timePast
+
+// For pyari Bike 3 🚲
+b3i.src = bike3.image
+b3p.innerHTML = bike3.price
+b3m.innerHTML = bike3.model
+b3l.innerHTML = bike3.location
+b3tp.innerHTML = bike3.timePast
+
+// For pyari Bike 4 🚲
+b4i.src = bike4.image
+b4p.innerHTML = bike4.price
+b4m.innerHTML = bike4.model
+b4l.innerHTML = bike4.location
+b4tp.innerHTML = bike4.timePast
+
+
+
+let superCars = [
+    car1 = {
+        price: "Rs 15,000,000",
+        model: "Lamborghini Huracan EVO 2023 Model",
+        location: "G-10, Islamabad",
+        timePast: "1 week ago",
+        image: "assets/car.png"
+    },
+    car2 = {
+        price: "Rs 18,000,000",
+        model: "Ferrari 488 GTB Top Condition",
+        location: "DHA Phase 5, Karachi",
+        timePast: "2 weeks ago",
+        image: "assets/car2.png"
+    },
+    car3 = {
+        price: "Rs 22,000,000",
+        model: "McLaren 720S Coupe",
+        location: "Blue Area, Islamabad",
+        timePast: "5 days ago",
+        image: "assets/car3.png"
+    },
+    car4 = {
+        price: "Rs 11,000,000",
+        model: "Porsche 911 Carrera 2021 Low Mileage",
+        location: "North Nazimabad, Karachi",
+        timePast: "2 weeks ago",
+        image: "assets/car4.png"
+    }
+];
+
+
+// For pyari car1 🚗
+c1p.innerHTML = car1.price
+c1m.innerHTML = car1.model
+c1l.innerHTML = car1.location
+c1tp.innerHTML = car1.timePast
+c1i.src = car1.image
+
+// For pyari car2 🚗
+c2p.innerHTML = car2.price
+c2m.innerHTML = car2.model
+c2l.innerHTML = car2.location
+c2tp.innerHTML = car2.timePast
+c2i.src = car2.image
+
+// For pyari car3 🚗
+c3p.innerHTML = car3.price
+c3m.innerHTML = car3.model
+c3l.innerHTML = car3.location
+c3tp.innerHTML = car3.timePast
+c3i.src = car3.image
+
+// For pyari car4 🚗
+c4p.innerHTML = car4.price
+c4m.innerHTML = car4.model
+c4l.innerHTML = car4.location
+c4tp.innerHTML = car4.timePast
+c4i.src = car4.image
+
+
+let pets = [
+    pet1 = {
+        price: "Rs 50,000",
+        type: "Persian Cat",
+        location: "G-10, Islamabad",
+        timePast: "1 week ago",
+        image: "assets/pet.png"
+    },
+    pet2 = {
+        price: "Rs 30,000",
+        type: "Golden Retriever Puppy",
+        location: "DHA Phase 5, Karachi",
+        timePast: "2 weeks ago",
+        image: "assets/pet2.png"
+    },
+    pet3 = {
+        price: "Rs 15,000",
+        type: "African Grey Parrot",
+        location: "Blue Area, Islamabad",
+        timePast: "5 days ago",
+        image: "assets/pet3.png"
+    },
+    pet4 = {
+        price: "Rs 10,000",
+        type: "Angora Rabbit",
+        location: "North Nazimabad, Karachi",
+        timePast: "2 weeks ago",
+        image: "assets/pet4.png"
+    }
+];
+
+// For pet1 🐕
+p1p.innerHTML = pet1.price
+p1type.innerHTML = pet1.type
+p1l.innerHTML = pet1.location
+p1tp.innerHTML = pet1.timePast
+p1i.src = pet1.image
+
+// For pet3 🐕
+p2p.innerHTML = pet2.price
+p2type.innerHTML = pet2.type
+p2l.innerHTML = pet2.location
+p2tp.innerHTML = pet2.timePast
+p2i.src = pet2.image
+
+// For pet3 🐕
+p3p.innerHTML = pet3.price
+p3type.innerHTML = pet3.type
+p3l.innerHTML = pet3.location
+p3tp.innerHTML = pet3.timePast
+p3i.src = pet3.image
+
+// For pet4 🐕
+p4p.innerHTML = pet4.price
+p4type.innerHTML = pet4.type
+p4l.innerHTML = pet4.location
+p4tp.innerHTML = pet4.timePast
+p4i.src = pet4.image
